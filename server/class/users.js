12bf5a47b42cd45a50f1aeba655b0e4cb9c43c3e
THE FABI7 <fabianguancha13@gmail.com>
2023-06-10 16:20:33 -0600

    user constructors

--- server/class/users.js
+++ server/class/users.js
@@ -4,12 +4,36 @@
 //     sala: 'videogames'
 // }
 
-
 class users {
-  
-    
+  // constructors
+  cosntructor() {
+    this.persons = [];
+  }
 
+  addPerson(id, name) {
+    let person = { id: id, name: name };
+    this.persons.push(person);
+    return this.persons;
+  }
 
+  getPerson(id) {
+    let person = this.persons.filter((person) => {
+      return person.id === id;
+    })[0];
+    return person;
+  }
+
+  getPersons() {
+    return this.persons;
+  }
+
+  deletePerson(id) {
+    let personDelete = this.getPerson(id);
+    this.persons = this.persons.filter((person) => {
+      person.id != id;
+    });
+    return personDelete;
+  }
 }
 
 module.exports = {
